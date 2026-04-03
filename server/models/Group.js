7eafc07ab20111f620193dd9569b4ab70a13af55
SirYipYap <97419787+SirYipYap@users.noreply.github.com>
2026-04-03 19:15:48 -0400

    Initial update to flesh out Group.js schema

--- server/models/Group.js
+++ server/models/Group.js
@@ -14,18 +14,38 @@ const groupSchema = new mongoose.Schema({
         required: true
     },
 
+    // Professor who manages this group
+    professorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
     // Which students are in this group
     members: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }],
 
+    // Tasks assigned to this group
+    tasks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task"
+    }],
+
     // Permissions for this group
     permissions: {
+        // If true, all members can add tasks
         canAddTasks: {
             type: Boolean,
             default: false
-        }
+        },
+
+        // If specified, only these students can add tasks
+        allowedStudents: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }]
     }
 
 }, { timestamps: true });
